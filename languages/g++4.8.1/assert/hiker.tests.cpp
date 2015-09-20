04abb39ba82d6aa3a86f0fd29238b27b01f78686
Matthew <matthew@bluefruit.co.uk>
2015-09-20 14:34:40 +0100

    Corrected everthing --> everything in G++ assert tests

--- languages/g++4.8.1/assert/hiker.tests.cpp
+++ languages/g++4.8.1/assert/hiker.tests.cpp
@@ -2,13 +2,13 @@
 #include <cassert>
 #include <iostream>
 
-static void life_the_universe_and_everthing()
+static void life_the_universe_and_everything()
 {
     assert(answer() == 42);
 }
 
 int main()
 {
-    life_the_universe_and_everthing();
+    life_the_universe_and_everything();
     std::cout << "All tests passed";
 }
